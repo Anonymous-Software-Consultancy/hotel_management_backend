b506b0658e4e9249115555b004da9c9fbbb06a7e
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-05-14 22:43:16 +0600

    new middlewares for new route named board-types

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
-import express, { Request, Response } from 'express';
-import helmet from 'helmet';
-import cors from 'cors';
-import 'reflect-metadata';
-require('dotenv').config();
+import express, { Request, Response } from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import 'reflect-metadata'
+require('dotenv').config()
 
-import userRoutes from './routes/userRoutes';
-import hotelRoutes from './routes/hotelRoutes';
-import facilityGroupRoutes from './routes/facilityGroupRoutes';
-import { errorMiddleWare } from './middlewares/errorMiddleware';
+import userRoutes from './routes/userRoutes'
+import hotelRoutes from './routes/hotelRoutes'
+import facilityGroupRoutes from './routes/facilityGroupRoutes'
+import boardTypes from './routes/boardTypeRoutes'
+import { errorMiddleWare } from './middlewares/errorMiddleware'
 
 const app = express()
 
@@ -18,9 +19,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // routes
-app.use('/users', userRoutes);
-app.use('/hotels', hotelRoutes);
+app.use('/users', userRoutes)
+app.use('/hotels', hotelRoutes)
 app.use('/hotels', facilityGroupRoutes)
+app.use('/board-types', boardTypes)
 
 // root
 app.get('/', async (_req: Request, res: Response) => {
@@ -31,6 +33,5 @@ const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-
 // error middleware
-app.use(errorMiddleWare);
+app.use(errorMiddleWare)
